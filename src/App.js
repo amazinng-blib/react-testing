5d37eb63beb55cbc124a64a8ccd975345f935d9d
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2023-11-07 17:07:09 +0100

    commented out test file line 7

--- src/App.js
+++ src/App.js
@@ -11,3 +11,26 @@ const App = () => {
 };
 
 export default App;
+
+// # name: Node.js CI
+
+// # on: [pull_request]
+
+// # jobs:
+// #   test:
+// #     runs-on: Windows-latest
+
+// #     strategy:
+// #       matrix:
+// #         node-version: [16.x]
+
+// #     steps:
+// #       - uses: actions/checkout@v2
+// #       - name: Use Node.js ${{ matrix.node-version }}
+// #         uses: actions/setup-node@v1
+// #         with:
+// #           node-version: ${{ matrix.node-version }}
+// #       - name: Setup Node
+// #         run: npm ci
+// #       - name: tests
+// #         run: npm test
