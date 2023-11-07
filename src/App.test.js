8db921e57cbf5d168b06a5b91cd5fc2b7651d5cf
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2023-11-07 15:45:09 +0100

    changed app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,9 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  // const text = screen.getByText(/ernest/i);
+  // const linkElement = screen.getByText(/learn react/i);
+  const text = screen.getByText(/ernest/i);
 
-  expect(linkElement).toBeInTheDocument();
+  expect(text).toBeInTheDocument();
   // expect(text).toBeInTheDocument();
 });
